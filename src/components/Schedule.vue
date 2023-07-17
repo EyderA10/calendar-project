36bf83449c1557c2c44b5c74db94efe94f33010d
EyderA10 <eyder@nubi.city>
2023-07-17 16:45:34 -0400

    calendar project with vue

---
+++ src/components/Schedule.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="div-calendar">
+        <h1>Schedule Here!</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, aliquam.s</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Schedule',
+    date() {
+        return {
+        }
+    },
+    mounted() {
+        console.log('Hello from Schedule component!')
+    }
+}
+</script>
+
+<style scoped>
+.div-calendar {
+    font-family: 'Montserrat';
+    font-weight: 700;
+    font-size: 18px;
+}
+</style>
